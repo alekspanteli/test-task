fcee9f549f95855d20f9477c2eb21221d5adb0c3
Alex Panteli <alekspanteli@gmail.com>
2025-07-24 00:58:41 +0300

    add unsubscribe and

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -1,30 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { IBook } from '../../models/book.model';
 import { InMemoryBookService } from '../../services/in-memory-book.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html'
 })
-export class BookListComponent {
+export class BookListComponent implements OnDestroy {
   books: IBook[] = [];
   notification = '';
+  private subscriptions: Subscription = new Subscription();
 
   constructor(private bookService: InMemoryBookService, private router: Router) {
-    this.bookService.getBooks().subscribe(data => this.books = data);
+    this.subscriptions.add(
+      this.bookService.getBooks().subscribe(data => this.books = data)
+    );
 
-    // Listen for navigation events to show notifications for add/edit
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        // Use window.history.state to get navigation state after navigation
-        if (window.history.state?.action === 'added') {
-          this.notification = 'Book added!';
-        } else if (window.history.state?.action === 'edited') {
-          this.notification = 'Book updated!';
+    this.subscriptions.add(
+      this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          // Use window.history.state to get navigation state after navigation
+          if (window.history.state?.action === 'added') {
+            this.notification = 'Book added!';
+          } else if (window.history.state?.action === 'edited') {
+            this.notification = 'Book updated!';
+          }
         }
-      }
-    });
+      })
+    );
   }
 
   editBook(id: number) {
@@ -39,9 +44,15 @@ export class BookListComponent {
 
   deleteBook(id: number) {
     if (confirm('Are you sure you want to delete this book?')) {
-      this.bookService.deleteBook(id).subscribe(() => {
-        this.notification = 'Book deleted!';
-      });
+      this.subscriptions.add(
+        this.bookService.deleteBook(id).subscribe(() => {
+          this.notification = 'Book deleted!';
+        })
+      );
     }
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
   }
 }
