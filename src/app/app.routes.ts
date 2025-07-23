e1a99b536c3140264cd493d2f1803093034f8785
Alex Panteli <alekspanteli@gmail.com>
2025-07-24 00:37:16 +0300

    lazy load routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
-import { BookListComponent } from './components/book-list/book-list.component';
-import { BookDetailComponent } from './components/book-detail/book-detail.component';
 import { BookResolver } from './resolvers/book.resolver';
 
 export const routes: Routes = [
-  { path: 'library', component: BookListComponent },
-  { path: 'library/:id', component: BookDetailComponent, resolve: { book: BookResolver } },
+  {
+    path: 'library',
+    loadComponent: () =>
+      import('./components/book-list/book-list.component').then(m => m.BookListComponent)
+  },
+  {
+    path: 'library/:id',
+    loadComponent: () =>
+      import('./components/book-detail/book-detail.component').then(m => m.BookDetailComponent),
+    resolve: { book: BookResolver }
+  },
   { path: '', redirectTo: '/library', pathMatch: 'full' }
 ];
